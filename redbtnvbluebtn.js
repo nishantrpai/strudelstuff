a6bea7f5b5d395cdf2b3095b2357e6c96fd88816
nishu <eiglesias914@gmail.com>
2026-06-04 21:46:05 +0530

    Update instrument and modify story notes

--- redbtnvbluebtn.js
+++ redbtnvbluebtn.js
@@ -2,46 +2,43 @@
 story: red btn v blue btn
 **/
 
-setcpm(72)
+setcpm(42)
 
-const INSTRUMENT = "supersaw"
+const INSTRUMENT = "piano"
 
 const story = [
   note("c2 eb2 g1 bb1"),
   note("c2 eb2 g1 bb1"),
 
   note("c2 f2 g1 bb1"),
-  note("c2 f2 g2 bb2"),
+  note("c2 f2 g1 bb1"),
 
-  note("c2 f2 g1 bb2"),
-  note("c2 f2 g2 bb1"),
+  note("c2 g2 g1 bb1"),
+  note("c2 g2 g1 bb1"),
 
-  note("c2 f2 g2 bb2"),
-  note("c2 f3 g2 bb1"),
+  note("c2 f2 g1 bb1"),
+  note("c2 f2 g1 bb1"),
 
-  note("c2 f2 eb2 bb2"),
-  note("c2 f2 eb2 bb1"),
+  note("c2 eb2 g1 bb1"),
+  note("c2 eb2 g1 bb1"),
 
-  note("c2 g2 eb2 bb2"),
-  note("c2 g2 eb2 bb1"),
-
-  note("f2 ~ eb3 ~"),
+  note("c2 ~ eb3 ~"),
   note("f3 ~ g2 ~"),
 
-  note("c2 ~ g3 ~"),
-  note("c3 ~ g2 ~"),
+  note("f2 ~ g3 ~"),
+  note("f2 ~ g2 ~"),
 ]
 
 // atmospheric streams
 const streams = [
-  note("eb2 ~ ~ c2"),
-  note("g2 ~ ~ eb2"),
-  note("bb1 ~ ~ g2"),
+  note("c2 ~ ~ f2"),
+  // note("g2 ~ ~ eb2"),
+  // note("bb1 ~ ~ g2"),
 
-  note("eb2 ~ ~ bb1"),
-  note("g2 ~ ~ c2"),
+  // note("g3 ~ ~ bb2"),
+  // note("g2 ~ ~ c2"),
 
-  note("bb2 ~ ~ eb2"),
+  // note("bb2 ~ ~ f2"),
 ]
 
 const echoes = [
@@ -62,81 +59,51 @@ stack(
 
   // MAIN STORY
   fastcat(story)
-
     .sound(INSTRUMENT)
-
     .slow(story.length)
-
-    .gain(0.28)
-
-    .lpf(850)
-
-    .room(0.12),
+    .gain(1)
+    .lpf(300)
+    .trans("32"),
 
   // STREAMS
-  fastcat(streams)
-
-    .sound("wt_digital_echoes")
-
-    .slow(streams.length * 2)
-
-    .gain(0.06)
-
-    .lpf(1200)
-
-    .room(0.45),
-
-  // LOW THREAT PULSE
-  // s("bd ~ ~ [bd bd]")
-
-  //   .bank("RolandTR909")
-
-  //   .slow(2)
-
-  //   .gain(0.35)
-
-  //   .lpf(100),
-
-  // TENSE DIGITAL TICKS
-  // s("~ hh ~ hh")
-
-  //   .bank("RolandTR808")
-
-  //   .gain(0.04)
-
-  //   .lpf(10),
-
-  // DISTANT GLITCH HITS
-  // s("~ wt_digital_curses ~ ~")
-
-  //   .slow(4)
-
-  //   .gain(0.08)
-
-  //   .room(0.7),
+  // fastcat(streams)
+  //   .sound("sawtooth")
+  //   .slow(streams.length * 2)
+  //   .gain(0.5)
+  //   .lpf(1000),
 
   // HIGH ECHOES
   fastcat(echoes)
-
     .sound("piano")
-
     .slow(echoes.length * 6)
+    .gain(0.24)
+    .lpf(1000)
+    .set.mix(`<1 1>*4`)
+    .trans("8")
+    .room(0.85),
 
-    .gain(0.04)
 
-    .room(0.55),
+  // LOW SYSTEM PULSE
+  
+  // LOW SYSTEM PULSE
+  s("bd ~ bd ~")
+    .bank("tr909")
+    .slow(4)
+    .gain(0.12)
+    .lpf(400),
 
-  // SUB DRONE
-  note("c0")
-
-    .sound("supersaw")
-
-    .slow(16)
-
+  // PROCESS TICKS
+  s("~ hh ~ hh")
+    .bank("tr909")
+    .slow(2)
     .gain(0.03)
+    .lpf(2500),
 
-    .lpf(70)
-
-    .room(0.2)
+  note("c2 eb2 g1 bb1")
+    .sound("piano")
+    .slow(128)
+    .gain(1)
+    .room(0)
+    .lpf("<10 300 500 300>"),
 
 )
