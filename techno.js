3e1754f0fda16042208a1054a5b50cb0ce9702c1
nishu <nishantrpai@gmail.com>
2026-06-13 21:50:16 +0530

    Refactor techno.js to use const and improve gain values

--- techno.js
+++ techno.js
@@ -1,19 +1,28 @@
 setcpm(124/4)
 
-$: n(run(16))
+const sawtooth = n(run(16))
     .scale("<c2 f2 eb2>:minor:pentatonic")
     .s("sawtooth")
     .delay(.7).orbit(2)
-    .gain(0.125)
+    .gain(0.5)
 
-$: s("bd:4!4").beat("0,4,8,11,14",16).duckorbit(2).duckattack(0.2).duckdepth(1).gain(0.25)
+const beats =  s("bd:4!4").beat("0,4,8,11,14",16).duckorbit(2).duckattack(0.2).duckdepth(1).gain(0.25)
 
 // 3. NEW: Driving Hi-Hats (Fills the high-end)
-$: s("hc!4").beat("2,6,10,14", 16).gain(0.15) // Classic off-beat techno hats
+const hh = s("hc!4").beat("2,6,10,14", 16).gain(0.15) // Classic off-beat techno hats
 
 // 4. NEW: Sub Bass (Follows your chord changes and ducks under the kick)
-$: n("<c1 f1 eb1>")
+const sine = n("<c1 f1 eb1>")
     .s("sine")
     .struct("1(3,8)") // Plays a driving, syncopated rhythm
-    .duckorbit(2).duckdepth(0.8) // Automatically ducks when the kick hits
-    .gain(0.3)
+    .duckorbit(2).duckdepth(0) // Automatically ducks when the kick hits
+    .gain(0.15)
+
+$: arrange(
+  [2, beats],
+  [4, stack(beats, sawtooth)],
+  [8, stack(beats, sawtooth, hh)],
+  [16, stack(beats, sawtooth, hh, sine)]
+)
+
+all(x => x._pianoroll())
