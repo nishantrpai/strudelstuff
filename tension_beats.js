eb2af9b57d74102efb4d1e9f7eaf1797da3df1bc
nishu <nishantrpai@gmail.com>
2026-06-25 12:57:35 +0530

    Refactor tension beats with new sound configurations

--- tension_beats.js
+++ tension_beats.js
@@ -1,11 +1,18 @@
 // was trying to make a beat for eissa, ended up here. https://x.com/PaiNishant/status/2069434123803062419
 setcpm(120/4)
 
-let beats = 
-  sound("bd*4,<brown brown brown>*8")
-      .decay(.04)
-      .sustain(0)
-      .gain(.25)
+// let beats = 
+//   sound("bd*4,<brown brown brown>*8")
+//       .decay(.04)
+//       .sustain(0)
+//       .gain(.25)
+
+let beats = (run(2))
+  .scale("<c2 f2>:minor:pentatonic*4")
+  .s("sbd")
+  .lpf(200)
+  .delay(.7)
+  .gain(0.5)
 
 const sawtones = n("[0 1 2] 3 4 5")
   .scale("<c2 f2 g2>:minor:pentatonic")
@@ -20,9 +27,10 @@ const sawpiano = n(run(4))
   .scale("<c2 f2 g2>:minor:pentatonic")
   .vib(1.01)
   .fast(1)
-  .s("sine")
+  .s("tri")
   .delay(0.7)
-  .lpf(500)
+  .lpf(200)
+  .gain(0.2)
 
 
 // gm_violin
@@ -38,9 +46,9 @@ const violinbeats = (run(2))
 const tribeats = (run(2))
   .scale("<c2 f2>:minor:pentatonic")
   .s("gm_overdriven_guitar")
+  .transpose(32)
   .delay(0.7)
   .lpf(500)
-
 
 
 
@@ -48,16 +56,15 @@ const tribeats = (run(2))
 let silence = s("piano").never()
 
 $: arrange(
-  [4, stack(beats, tribeats)],
-  [4, stack(beats, sawtones, tribeats)],
-  [32, stack(beats, sawtones, violinbeats, tribeats)]
-  
+  // [2, stack(beats).gain(0.1)],
+  // [2, stack(beats, tribeats, sawtones.slow(4)).gain(0.1)],
+  // [8, stack(beats, tribeats, sawtones.slow(2)).gain(0.2)],
+  // [4, stack(beats, sawtones, tribeats)],
+  [32, stack(beats, sawtones, violinbeats, tribeats.gain(0.45), sawpiano)],
+  // [2, stack(beats, sawtones, violinbeats, tribeats.gain(0.45), sawpiano).gain(segment(8, range(0.4, 0, saw)))],
   // [999, silence]
 )
 
-
-
-all(x => x._pianoroll())
 
 
 all(x => x._pianoroll())
