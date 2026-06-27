62d9013304d1358464f95f261dc0c104d8773f17
nishu <nishantrpai@gmail.com>
2026-06-27 14:38:23 +0530

    Add happybeat.js with sound note definitions

---
+++ happybeat.js
@@ -0,0 +1,35 @@
+//random
+
+$: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
+  .sound("gm_overdriven_guitar")
+  .n("10 [0 1] 2")
+  .fast(2)
+  .transpose(32)
+  .lpf(sine.range(300, 800).slow(2))
+  .gain(0.5)
+
+
+$: sound("bd:4*4").gain(0.2)
+
+
+$: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
+  .sound("piano")
+  .n("10 [0 1] 2")
+  .fast(1)
+  .transpose(32)
+  .lpf(sine.range(300, 800).slow(2)) 
+  .gain(0.25)
+
+
+$: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
+  .sound("gm_xylophone")
+  .n("10 [0 1] 2")
+  .vowel("<a e i o>*8")
+  .fast(2)
+  .transpose(32)
+  .lpf(sine.range(300, 800).slow(2))
+  .gain(0.35)
+  .rev()
+
+
+all(x => x._pianoroll())
