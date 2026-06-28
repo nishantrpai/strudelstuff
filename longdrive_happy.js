83d093614a910a2da40e82db956a3e5f3197b76d
nishu <nishantrpai@gmail.com>
2026-06-28 15:45:03 +0530

    Refactor longdrive_happy.js for clarity and structure

--- longdrive_happy.js
+++ longdrive_happy.js
@@ -1,7 +1,7 @@
-//mental reference: going on  long drive, dancing and enjoying the mood
+//random
 samples('github:bubobubobubobubo/dough-waveforms')
 
-$: note("[c2 d2!2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
+const dancelead = note("[c2 d2!2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
   .sound("gm_overdriven_guitar")
   .n("10 [0 1] 2")
   .fast(2)
@@ -13,10 +13,10 @@ $: note("[c2 d2!2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
 
 
 // todo: intro*2 main*4
-$: sound("[bd:4 bd:2]*2").gain(0.2)
+const dancebeat = sound("[bd:4 bd:2]*2").gain(0.15)
 
 
-$: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
+const danceref1 = note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
   .sound("piano")
   .n("10 [0 1] 2")
   .fast(1)
@@ -25,7 +25,7 @@ $: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
   .gain(0.25)
 
 
-$: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
+const danceref2 = note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
   .sound("gm_xylophone")
   .n("10 [0 1] 2")
   .vowel("<a e i o>*8")
@@ -36,5 +36,27 @@ $: note("[c2 d2 bb2]  ~ [f2 g2 bb2] ~ [eb2, g2]")
   .rev()
 
 
+const silence = s("piano").never()
+
+$: arrange(
+  // hi
+  [2, stack(dancelead.delay(0), danceref1)],
+  // slow beat
+  [4, stack(dancelead.delay(0.1), danceref1, dancebeat.sound("[bd:4 hh:2]*2"))],
+  // fast beat
+  [8, stack(dancelead, danceref1, dancebeat)],
+  // main
+  [16, stack(dancelead, danceref1, dancebeat)],
+  // peak
+  [32, stack(dancelead.degradeBy(0.1), danceref1, danceref2, dancebeat)],
+  // slowdown
+  [4, stack(dancelead.delay(0.1), danceref1, dancebeat.sound("[bd:4 hh:2]*2"))],
+  // k.bye
+  [2, stack(dancelead.delay(0), danceref1)],
+  // exit
+  [999, silence]
+)
 
 all(x => x._pianoroll())
+
+
