03504e20ac9eeefae82dce1b87fafb370a55bdd9
nishu <nishantrpai@gmail.com>
2026-06-30 17:47:53 +0530

    Fix horse image properties and append logic
    Updated image transparency and fixed class assignment for horse background.

--- horserunning.js
+++ horserunning.js
@@ -2,20 +2,26 @@
 // visual reference: https://giphy.com/gifs/masterpiecepbs-horse-horses-horseback-riding-7T40uVfFeDrKz4Ik2j
 
 samples('github:bubobubobubobubo/dough-waveforms')
-const img = document.createElement('img');
-img.src = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExejg0c2RjM3AxZWk2ZnpkanllZWl4cHZkM280d3VoZGptb21hdHMzdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7T40uVfFeDrKz4Ik2j/giphy.gif'; // Replace with your direct URL
+
+let img = document.querySelector('.horse-bg');
+
+if (!img) {
+  // If it doesn't exist, create it once
+  img = document.createElement('img');
+  img.className = 'horse-bg'; // Fixed: Use className instead of class
+  document.body.appendChild(img);
+}
+
+// These properties will now safely update every time you press Run
+img.src = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExejg0c2RjM3AxZWk2ZnpkanllZWl4cHZkM280d3VoZGptb21hdHMzdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7T40uVfFeDrKz4Ik2j/giphy.gif'; 
 img.style.position = 'fixed';
 img.style.top = '0';
 img.style.left = '0';
 img.style.width = '100vw';
 img.style.height = '100vh';
 img.style.objectFit = 'cover';
-img.style.zIndex = '-1'; // Puts it safely behind the text editor
-img.style.opacity = '0.2'; // Adjust to keep text readable
-
-// Append to the workspace page
-document.body.appendChild(img);
-
+img.style.zIndex = '-1'; 
+img.style.opacity = '0.1'; // Change this number to instantly adjust transparency!
 
 
 $: note("[~ [<[d3,a3,f4]!3 [d3,bb3,g4]!1> ~]]*2")
@@ -27,9 +33,10 @@ $: note("[~ [<[d3,a3,f4]!3 [d3,bb3,g4]!1> ~]]*2")
   .release(.2)
   .delay(.5)
   .gain(.23)
+  .degradeBy(0.01)
 
 
-$: note("[~ c2 ~ f2]").sound("bd:4").lpf(100).fast(2).gain(.125)
+$: note("[~ c2 ~ f2]").sound("bd:2").lpf(100).fast(2).gain(.125)
 
 
 all(x => x._pianoroll())
