90075a1cba4ed06589d477b9aca9960c0c5bcbb2
nishu <nishantrpai@gmail.com>
2026-07-11 08:22:10 +0530

    Modify sound settings and note parameters in d36.js

--- d36.js
+++ d36.js
@@ -2,13 +2,14 @@
 
 $: note
   ("[E3,G3,B3][E4,G4,B4]")
-  .s("supersaw").delay(0.5).decay(0.5)
-  .scale("<c1 f5>:minor:pentatonic")
+  .s("supersaw").delay(1).decay(0.5).n(run(2))
+  .scale("<c1 f5>:minor:pentatonic").gain(0.5)
 
 $: s("[bd:4 bd:6]!2")
 
-$: note(`c2 f2`).s("gm_distortion_guitar")
+$: note(`c f <b , g> e`).s("sawtooth")
   .scale("<c3 f3>:minor:pentatonic")
-  .lpenv(100)
-  .lpf(500)
-  .fast(4)
+  .lpenv(500)
+  .lpf(2000)
+  .slow(2)
+  .delay(.5)
