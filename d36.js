b25910657fcb0455a4feee3429424eb770db6f5e
nishu <nishantrpai@gmail.com>
2026-07-18 21:16:39 +0530

    Update image source and styling in d36.js

--- d36.js
+++ d36.js
@@ -11,21 +11,21 @@ if (!img) {
 }
 
 // These properties will now safely update every time you press Run
-img.src = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZHV0c3VzazE0N2J1NHJveXg4aThwZzc0cm84eTZpbmozZWJmYXd6ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RKMXPlnuViOnZTIUlk/giphy.gif'; 
+img.src = 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWVlcHFuYjh0OHZ4ejNlYjYyYzdiY3J4M3NobTFmcml4MTBkNmhweCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IxwbtFKGFxgac/giphy.gif'; 
 img.style.position = 'fixed';
 img.style.top = '0';
 img.style.right = '0';
-img.style.width = '50vw';
-img.style.height = '50vh';
+img.style.width = '100vw';
+img.style.height = '100vh';
 img.style.objectFit = 'cover';
 img.style.zIndex = '-1'; 
-img.style.opacity = '0.5'; // Change this number to instantly adjust transparency!
+img.style.opacity = '0.7'; // Change this number to instantly adjust transparency!
 
 
 
 const pad = note
   ("[E3,G3,B3][E4,G4,B4]")
-  .s("supersaw")
+  .s("gm_kalimba")
   .delay(1)
   .decay(0.75)
   .scale("<c1 f5>:minor:pentatonic").gain(0.5)
@@ -45,19 +45,12 @@ const buildup = s("supersaw").seg(16).lpf(tri.range(100, 200).slow(2))
 const silence = s("bd").never()
 
 $: arrange(
-  [2,
-    pad.fast(4)
-  ],
-  [2,
+  [4,
     stack(pad.fast(4), beat)
   ],
   [4,
     stack(pad.fast(4), beat, pad2.s("bd:6"))
   ],
-  [4,
-    stack(pad.fast(4), beat, pad2.fast(2))
-  ],
-  //good buildup
   [4,
     stack(pad.fast(4), beat, pad2.fast(4))
   ],
@@ -68,10 +61,10 @@ $: arrange(
     stack(pad.fast(4), beat, pad2.s("saw"), buildup)
   ],
   [4,
-    stack(pad.fast(4), beat, pad2.s("saw").delay(2), buildup)
+    stack(pad.s('supersaw').delay(.5).fast(4), beat, pad2.s("saw").delay(2), buildup)
   ],
-    [4,
-    stack(pad.fast(4), beat, pad2.s("saw").fast(4), buildup.fast(4))
+  [8,
+    stack(pad.s('supersaw').fast(4), beat, pad2.s("saw").fast(4), buildup.fast(4))
   ],
 
 )
