e1119d3bbee95036cceec685087be28dfc0a5f18
nishu <nishantrpai@gmail.com>
2026-08-08 18:33:56 +0530

    Refactor sound settings and instruments in d50_unsettling.js
    Updated sound settings and instrument choices for better audio quality.

--- d50_unsettling.js
+++ d50_unsettling.js
@@ -1,23 +1,24 @@
-
 $: n(run(8))
   .chord("<C <Fm Db>>")
   .voicing()
-  .s('saw')
+  .s('supersaw')
   .fast(2)
-  .adsr(".002")
-  .gain(.5)
-  .lpf(700)
+  .adsr(1/8)
+  .gain(.5) // Reduced from .5
+  .lpf(800)
 
-
-$: n(`[3,1,2]!2`)
+$: n(`[3,1,2]`)
   .chord("<C <Fm Db>>")
   .voicing()
-  .s('supersaw')
+  .s('gm_drawbar_organ')
   .fast(.5)
-  .gain(.5)
-  .lpf(700)
+  .gain(.5) // Reduced from .5 (supersaw is naturally very loud)
+  .lpf(800)
 
-$: s("[bd:6 bd:4]*2").lpf(100).adsr(".1")
+$: s("[bd:6 bd:4]!2").lpf(500).gain(.85) // Added a controlled gain for the kick
 
 
+$: note("c4 <f4 db4>").s("piano").slow(2).lpf(800)
+
+// This scales down the total summed output to prevent clipping
 all(x => x._pianoroll())
