9a5c325c8121b977d1a907ea20a2ed7baa52d1f6
nishu <nishantrpai@gmail.com>
2026-08-09 17:50:07 +0530

    Modify sound sources and filter settings
    Updated sound settings and improved note structure.

--- d50_unsettling.js
+++ d50_unsettling.js
@@ -1,24 +1,25 @@
 $: n(run(8))
   .chord("<C <Fm Db>>")
   .voicing()
-  .s('supersaw')
+  .s('saw')
   .fast(2)
   .adsr(1/8)
   .gain(.5) // Reduced from .5
-  .lpf(800)
+  .lpf(1200)
 
 $: n(`[3,1,2]`)
   .chord("<C <Fm Db>>")
   .voicing()
-  .s('gm_drawbar_organ')
+  .s('gm_contrabass')
   .fast(.5)
-  .gain(.5) // Reduced from .5 (supersaw is naturally very loud)
+  .gain(.75) // Reduced from .5 (supersaw is naturally very loud)
   .lpf(800)
 
 $: s("[bd:6 bd:4]!2").lpf(500).gain(.85) // Added a controlled gain for the kick
 
 
-$: note("c4 <f4 db4>").s("piano").slow(2).lpf(800)
+$: note("[c4, f4] <[f4, db4] [f4,g4]>").s("piano").slow(2).lpf(1200)
 
-// This scales down the total summed output to prevent clipping
+
+
 all(x => x._pianoroll())
