c4bce43f6af6f96c5d4a392d253f66cca4e63c60
nishu <nishantrpai@gmail.com>
2026-08-16 18:17:41 +0530

    Enhance bird sound sequences with ADSR effects

--- birds.js
+++ birds.js
@@ -4,12 +4,12 @@ references:
 - https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F0f%2F56%2F86%2F0f5686229feb07c22b0afe3ab3a51de9.jpg&f=1&nofb=1&ipt=d25799b963d256dd0f5f185780a7c05d4b25a0d63af61f68d6f1cd8f698f419d
 */
 
-
 // everything was accidental as always
 // this sequence is to signify the forces the birds travels against: winds/gravity/universe
 // i liked this instrument, both instruments are reliant on wind
 // i have never played or seen this one, but v much enjoy this. was thinking of piano, but this made more sense
-$: note("c4@2 c#4  f##4,f2 f4,f4 c4").sound("gm_shakuhachi").slow(2)
+$: sound("bd:4*4")
+$: note("c4@2 c#4  f##4,f2 f4,f4 c4").sound("gm_shakuhachi").slow(2).adsr(".5")
 
 // this sequence is its flight path
 // this entire sequence is one cycle
@@ -17,5 +17,5 @@ $: note("c4@2 c#4  f##4,f2 f4,f4 c4").sound("gm_shakuhachi").slow(2)
 // that V is at the beginning and end signifying take off and during landing/repeating
 // then there are sequences where it is !2 those are where the wings are down 
 // in a way the wings are playing the tune with universe
-$: note("c4@2 c#4!2 c3!2 f3!2 f4@2").n(run(4)).sound("gm_flute").fast(1)
+$: note("c4@2 c#4!2 c3!2 f3!2 f4@2").n(run(4)).sound("gm_flute").fast(1).adsr(".5")
 all(x => x._pianoroll())
